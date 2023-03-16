bc37ac678d98bdaf586796d710f5c99f7e545b75
KmolYuan <pyslvs@gmail.com>
2023-03-16 12:01:27 +0800

    Update progress.

--- src/main.rs
+++ src/main.rs
@@ -27,37 +27,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     const PATH: &[&str] = &[
         "../four-bar-rs/syn-examples/crunode.closed.ron",
         "../four-bar-rs/syn-examples/cusp.closed.ron",
+        "../four-bar-rs/syn-examples/heart.closed.ron",
         "../four-bar-rs/syn-examples/c-shape.open.ron",
         "../four-bar-rs/syn-examples/sharp.open.ron",
-        "../four-bar-rs/syn-examples/heart.closed.ron",
     ];
-    let fb = ron::from_str::<FourBar>(&std::fs::read_to_string(PATH[1])?)?;
+    let fb = ron::from_str::<FourBar>(&std::fs::read_to_string(PATH[2])?)?;
+    // let mut fb = FourBar::example();
+    // *fb.a_mut() = 10f64.to_radians();
+    // *fb.inv_mut() = true;
     let path = fb.curve(360);
     let path_closed = curve::closed_lin(&path);
     let efd = efd::Efd2::from_curve_harmonic(&path_closed, None).unwrap();
     let harmonic = efd.harmonic();
-    let fft_recon = fft_recon(&path_closed[..path_closed.len() - 1], harmonic);
+    let harmonic2 = harmonic * 2;
+    let fft_recon = fft_recon(&path_closed[..path_closed.len() - 1], harmonic2);
     let path_recon = efd.generate(180);
     let b = plot2d::SVGBackend::new("test.svg", (800, 800));
     plot2d::plot(
         b,
         [
             ("Original", path.as_slice()),
-            (
-                &format!("EFD Reconstructed ({harmonic} harmonics)"),
-                path_recon.as_slice(),
-            ),
-            (
-                &format!("FD Reconstructed ({harmonic} harmonics)"),
-                fft_recon.as_slice(),
-            ),
+            ("EFD Reconstructed", path_recon.as_slice()),
+            ("FD Reconstructed", fft_recon.as_slice()),
         ],
         plot2d::Opt::from(None)
-            // .scale_bar(1.)
-            // .axis(false)
+            .scale_bar(10.)
+            .axis(false)
             .grid(false)
             .dot(true)
-            .stroke(4),
+            .font(35.)
+            .stroke(10),
     )?;
     Ok(())
 }
