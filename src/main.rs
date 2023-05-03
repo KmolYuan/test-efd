92f4d5753c7c286da58ed3eface00ad621f417f5
KmolYuan <pyslvs@gmail.com>
2023-05-03 23:49:06 +0800

    Add fitting approach.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use four_bar::{plot2d::IntoDrawingArea, *};
+use four_bar::{efd, efd::na, plot2d, plot2d::IntoDrawingArea, FourBar};
+use std::f64::consts::PI;
 
 fn fft_recon(path: &[[f64; 2]], harmonic: usize) -> Vec<[f64; 2]> {
     use rustfft::{num_complex::Complex, num_traits::Zero as _};
@@ -33,10 +34,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ];
     let fb = ron::from_str::<FourBar>(&std::fs::read_to_string(PATH[4])?)?;
     let path = fb.curve(360);
-    let efd = efd::Efd2::from_curve(&path, true);
-    let harmonic = efd.harmonic();
+
+    // Drect method
+    let harmonic = 19;
+    let t0 = std::time::Instant::now();
+    let efd = efd::Efd2::from_curve_harmonic(&path, true, None);
+    dbg!(t0.elapsed());
     let fft_recon = fft_recon(&path, harmonic * 2);
     let path_recon = efd.generate(180);
+
+    let efd_fitting_recon = {
+        let harmonic = path.len() / 2;
+        let t0 = std::time::Instant::now();
+        let theta = na::RowDVector::from_fn(path.len(), |_, i| i as f64 / path.len() as f64 * PI);
+        let ax = na::MatrixXx2::from_row_iterator(path.len(), path.iter().flatten().copied());
+        let mut a = na::DMatrix::zeros(2 * harmonic, path.len());
+        for r in 0..harmonic {
+            let n = (r + 1) as f64;
+            a.row_mut(r).copy_from(&(n * &theta).map(f64::cos));
+            a.row_mut(r + harmonic)
+                .copy_from(&(n * &theta).map(f64::sin));
+        }
+        let a2 = a.transpose();
+        let omega = &a * a2;
+        let y = a * ax;
+        let x = omega.lu().solve(&y).unwrap();
+        let coeffs = na::MatrixXx4::from_columns(&[
+            x.column(0).rows(0, harmonic),
+            x.column(0).rows(harmonic, harmonic),
+            x.column(1).rows(0, harmonic),
+            x.column(1).rows(harmonic, harmonic),
+        ])
+        .transpose();
+        let efd2 = efd::Efd2::try_from_coeffs(coeffs).unwrap();
+        dbg!(t0.elapsed());
+        efd2.generate_half(360)
+    };
+
+    // Plot
     let b = plot2d::SVGBackend::new("test.svg", (800 * 3, 800));
     let mut roots = b.into_drawing_area().split_evenly((1, 3));
     let [root1, root2, root3] = [roots.remove(0), roots.remove(0), roots.remove(0)];
@@ -50,7 +85,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     )?;
     let opt = plot2d::Opt::new()
         .grid(false)
-        .font(50.)
+        .font(20.)
         .dot(true)
         .legend(plot2d::LegendPos::MM);
     plot2d::plot(
@@ -58,6 +93,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         [
             ("Original", path.as_slice()),
             ("EFD Reconstructed", path_recon.as_slice()),
+            ("EFD Fitting Reconstructed", efd_fitting_recon.as_slice()),
         ],
         opt.clone(),
     )?;
